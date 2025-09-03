cf677c02ea0979911f5edca1f46d092b67f51740
ShDhz <zombiephoenix43@gmail.com>
2025-09-03 21:23:10 +0700

    remove middleware admin ini admin route group

--- routes/admin.php
+++ routes/admin.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\admin\tentangBumdesController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware(['auth', 'verified', 'admin'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard-admin', [dashboardAdminController::class, 'index'])->name('dashboard.admin');
     Route::prefix('profil')->group(function () {
         Route::resource('data-umum', DataUmumController::class);
